1f06e5057c4d388be83c84044cc65d91840ddb87
Vaja Vishvas <vaja28065@gmail.com>
2025-05-30 04:35:51 -0700

    Add files via upload

---
+++ MODULE-C PROGRAMING/TASK/task5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float base,hieght,area;
+    //float PI=3.14;
+    
+    printf("Enter triangle base:");
+    scanf("%f",&base);
+    
+    printf("Enter triangle hieghrt:");
+    scanf("%f",&hieght);
+    
+    area = (base*hieght) / 2 ;
+    
+    printf("Area of Circle is %f",area);
+    
+    
+    getch();
+}
