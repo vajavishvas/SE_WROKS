0cc330bd2b0748feeafc904e1487c6af6c81fc0c
Vaja Vishvas <vaja28065@gmail.com>
2025-05-30 02:06:17 -0700

    Add files via upload

---
+++ MODULE-C PROGRAMING/main (4).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a = 7;
+    int b = 4;
+     
+    int ans = a%b; // 7 % 4 
+    
+    printf(" modulus is %d ",ans);
+    
+    getch();
+}
