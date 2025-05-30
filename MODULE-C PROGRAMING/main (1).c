b7eb8c24148cc9604af4fedb78d9ce017ce2c65c
Vaja Vishvas <vaja28065@gmail.com>
2025-05-30 01:59:04 -0700

    Add files via upload

---
+++ MODULE-C PROGRAMING/main (1).c
@@ -0,0 +1,13 @@
+#include<stdio.h>// header file
+#include<conio.h>// header file
+void main()//main function 
+{
+   int num1 = 7;
+   int num2 = 8;
+   
+   printf(" \n Number 1 is : %d",num1);
+   printf(" \n Number 2 is : %d",num2);
+   
+    
+    getch();// terminate your prongram
+}
