7444b3a3275dc408804e6e5c102ff72db0c695a5
Vaja Vishvas <vaja28065@gmail.com>
2025-05-30 01:58:37 -0700

    Add files via upload

---
+++ MODULE-C PROGRAMING/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>// header file
+#include<conio.h>// header file
+void main()//main function 
+{
+    printf(" \n my name : vaja vishvas \t my age : 19");
+    printf("\n vaja vishvas ");
+    
+    getch();// terminate your prongram
+}
