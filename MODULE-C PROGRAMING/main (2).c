7e9f0f9fc9904054e13e823bd60c67d94e66d79f
Vaja Vishvas <vaja28065@gmail.com>
2025-05-30 01:59:27 -0700

    Add files via upload

---
+++ MODULE-C PROGRAMING/main (2).c
@@ -0,0 +1,19 @@
+#include<stdio.h>// header file
+#include<conio.h>// header file
+void main()//main function
+{
+	int a = 7;
+	float b = 45.34;
+	char c = 'a';
+	double d = 34.78468;
+
+	printf("\n your integer value is %d",a);
+	printf("\n your float value is %f",b);
+	printf("\n your character value is %c",c);
+    printf("\n your double value is %lf",d);
+
+
+
+
+	getch();// terminate your prongram
+}
