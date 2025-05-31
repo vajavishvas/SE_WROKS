825ae5fc46b3ee069cf0167aec066ca43d1b58f2
Vaja Vishvas <vaja28065@gmail.com>
2025-05-30 22:41:49 -0700

    Add files via upload

---
+++ MODULE-C PROGRAMING/main (6).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    
+    printf(" Enter your age ");
+    scanf("%d",&a);
+    
+    if(a>=18)
+    {
+        printf(" \n aligebal to vote");
+    }
+    else
+    {
+        printf(" \n dont'n aligebal to vote");
+        
+    }
+    
+    getch();
+}
