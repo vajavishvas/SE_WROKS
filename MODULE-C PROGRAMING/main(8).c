8805580480963e44b53700ff0dee8ea300981333
Vaja Vishvas <vaja28065@gmail.com>
2025-05-30 22:44:12 -0700

    Add files via upload

---
+++ MODULE-C PROGRAMING/main(8).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    
+    printf(" Enter your number ");
+    scanf("%d",&a);
+    
+    if(a>0)
+    {
+        printf(" \n pogetiv ");
+    }
+    else
+    {
+        printf(" \n negetiv");
+        
+    }
+    
+    getch();
+}
