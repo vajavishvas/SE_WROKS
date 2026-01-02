a41b8592db5222b2add646c682c05fd7c07923a8
Vaja Vishvas <vaja28065@gmail.com>
2026-01-02 05:46:12 -0800

    COMMIT

---
+++ 5 TO 6-30 BANCH/8-12/main (1).c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a = 4;
+    int b = 2;
+    
+    
+    int add = a+b;
+    int sub = a-b;
+    int mul = a*b;
+    int div1 = a/b;
+    int mod = a%b;
+    
+    printf("\n Your Addition is : %d",add);
+    printf("\n Your Substraction is : %d",sub);
+    printf("\n Your Multiplication is : %d",mul);
+    printf("\n Your Division is : %d",div1);
+    printf("\n Your Modulus is : %d",mod);
+    
+    
+    getch();
+}
---
+++ 5 TO 6-30 BANCH/8-12/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a = 5;
+    int b = 3;
+    
+    printf("\n Your value for a is : %d",a);
+    printf("\n Your value for b is : %d",b);    
+
+    getch();
+}
